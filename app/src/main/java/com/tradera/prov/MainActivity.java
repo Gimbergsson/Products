5dad23ad035abcd503f9d24de9755a8e11186152
Dennis Gimbergsson <dennisgimbergsson@live.se>
2017-11-27 21:39:13 +0100

    removed onDestroy

--- app/src/main/java/com/tradera/prov/MainActivity.java
+++ app/src/main/java/com/tradera/prov/MainActivity.java
@@ -59,11 +59,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
